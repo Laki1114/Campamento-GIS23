aa00244ff4831d753f1a191412bb2e4e1f1898d7
Laki1114 <2021is052@stu.ucsc.cmb.ac.lk>
2024-02-16 23:05:18 +0530

    login form

---
+++ login/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+
+
+<div class="bg-img">
+  <form action="/action_page.php" class="container">
+    <h1>Login to your Account</h1> <br>
+
+    <label for="email"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <input type="submit" value="Login"> <br>
+
+    <label style="align:center;">
+     <input type="checkbox" checked="checked" name="remember"> Remember me
+     </label>
+<br><br><br>
+     <div >
+        <button type="button" class="cancelbtn">Cancel</button>
+        <span class="psw">Forgot <a href="forgotpw.html">password?</a></span>
+        </div>
+
+        <br><br>
+        <h3>New Here?</h3> <br>
+
+        <center><button type="button" class="signbtn">Signup</button></center>
+
+  </form>
+</div>
+
+</body>
+</html>
