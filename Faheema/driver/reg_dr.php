def4f7000360e18579dbfb7dfa1d2788ace319fd
faheemafrauz <frauzfaheema@gmail.com>
2024-02-19 20:55:16 +0530

    Update reg_dr.php

--- Faheema/driver/reg_dr.php
+++ Faheema/driver/reg_dr.php
@@ -24,126 +24,134 @@ if(isset($_POST['regbtn'])){
 	//code for image uploading
   $targetDir = "pic/";  // Specify the directory where you want to save the uploaded images
   $targetDirr = "license/";
-  $targetDire = "veh/";
+  $targetDire = "vehicle/";
 
   // Handle the first image
-  $targetFile1 = $targetDir . basename($_FILES["pic"]["name"]);
-  move_uploaded_file($_FILES["pic"]["tmp_name"], $targetFile1);
+  $targetFile1 = $targetDir . basename($_FILES["picture"]["name"]);
+  move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFile1);
 
-  $targetFile2 = $targetDire . basename($_FILES["veh"]["name"]);
-  move_uploaded_file($_FILES["veh"]["tmp_name"], $targetFile2);
+  $targetFile2 = $targetDire . basename($_FILES["vehicle"]["name"]);
+  move_uploaded_file($_FILES["vehicle"]["tmp_name"], $targetFile2);
   // Handle the second image
   $targetFile3 = $targetDirr . basename($_FILES["license"]["name"]);
   move_uploaded_file($_FILES["license"]["tmp_name"], $targetFile3);
  
  
-  $password = md5($ps);
+  //$password = md5($ps);
 
-  $i="insert into driver(firstname, lastname, gender, phone, address,veh_des, vehicle, picture, experience, nic, email, password, location, license, qualification)values ('$f','$l','$g','$p','$a','$v','$targetFile2','$targetFile1','$e', '$n','$ema','$password','$lo','$targetFile3', '$q')";
+  $i="insert into driver(firstname, lastname, gender, phone, address,veh_des, vehicle, picture, experience, nic, email, password, location, license, qualification)values ('$f','$l','$g','$p','$a','$v','$targetFile2','$targetFile1','$e', '$n','$ema','$ps','$lo','$targetFile3', '$q')";
   if(mysqli_query($con, $i)){
 	$msg = "Account created";
-	header("Location: drvr.php?warning=" . urlencode($msg));
+	header("Location:../../home?warning=" . urlencode($msg));
 	exit();
   }
 }
 ?>
+
 <!DOCTYPE html>
+<html>
 <head>
-    <link rel="stylesheet" href="registrd.css">
-    <title></title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="registerUser.css">
 </head>
 <body>
 
-    <form method="post" enctype="multipart/form-data">
-    <table>
+
+<div class="bg-img">
+  
+  <form method="post" enctype="multipart/form-data" class="container">
+  <br><br><h1>Register as Driver</h1><br>
+    <br><br>
+    <p><center>Please fill in this form to create an account.</center></p><br>
+  <table>
         <tr> 
-            <td>
-                <!--start left panel-->
-               
-                    <div class="container">
-                      <h1>Register as a Driver</h1>
-                      <p>Please fill in this form to create an account.</p>
-                      <hr>
- 
+            <td width="300px">
+            <div class="container-left">
+
+    <label for="firstName"><b>First Name </b></label><br>
+    <input type="text" placeholder="First Name" name="firstname" id="firstName" required><br>
+                  
+    <label for="lastName"><b>Last Name </b></label><br>
+    <input type="text" placeholder="Last Name" name="lastname" id="lastName" required><br>
+
+    <label for="gender"><b>Gender</b></label><br>
+     <select id="gender" name="gender" required>
+     <option value="" disabled selected>Select Gender</option>
+    <option>Male</option>
+      <option>Female</option>
+      <option>Other</option>
+     </select><br>
     
-                      <label for="firstName"><b>First Name </b></label><br>
-                      <input type="text" placeholder="First Name" name="firstName" id="firstName" required><br>
-                  
-                      <label for="lastName"><b>Last Name </b></label><br>
-                      <input type="text" placeholder="Last Name" name="lastName" id="lastName" required><br>
 
-            
-                      <label for="gender"><b>Gender</b></label><br>
-                      <select id="gender" name="gender" required>
-                        <option value="" disabled selected>Select Gender</option>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                      </select><br>
-                  
-                      <label for="phoneNo"><b>Phone No </b></label><br>
-                      <input type="text" placeholder="Phone No" name="phone"  required><br>
+     <label for="phoneNo"><b>Phone No </b></label><br>
+      <input type="text" placeholder="Phone No" name="phone" id="phone" required><br>
 
-                      <label for="address"><b>Address </b></label><br>
-                      <input type="text" placeholder="Address" name="address" required><br>
+      <label for="address"><b>Address </b></label><br>
+      <input type="text" placeholder="Address" name="address" required><br>
 
-                      <label for="photo"><b>Picture </b></label><br><br>
-                      <input type="file" name="picture" id="pic"><br><br>
+      <label for="photo"><b>Picture </b></label><br><br>
+      <input type="file" name="picture" id="pic"><br><br>
 
-                      <label for="vehicle-des"><b>Vehicle Description </b></label><br>
-                      <input type="text" placeholder="Vehicle des" name="vehicleDes" required><br>
+      <label for="vehicle-des"><b>Vehicle Description </b></label><br>
+       <input type="text" placeholder="Vehicle des" name="vehicleDes" required><br>
 
-                      <label for="Vehicle"><b>Vehicle Pictures </b></label><br><br>
-                    <input type="file" name="vehicle" id="vehicle">
+      <label for="Vehicle"><b>Vehicle Pictures </b></label><br><br>
+      <input type="file" name="vehicle" id="vehicle">
 
-            </td>
+</div>
+      </td>
 
-            <!--start right panel-->
+<!--start right panel-->
 
-            <td width="450px">
+<td width="300px">
+<div class="container-right">
+<br><br>
+<label for="phoneNo"><b>NIC No </b></label><br>
+ <input type="text" placeholder="NIC No" name="nic" id="nic" required><br>
+
+  <label for="email"><b>Email</b></label><br>
+   <input type="text" placeholder="Enter Email" name="email" id="email" required><br>
                 
-                <div class="container-right">
-                    <br><br><br><label for="phoneNo"><b>NIC No </b></label><br>
-                    <input type="text" placeholder="NIC No" name="nic" required><br>
-
-                    <label for="email"><b>Email</b></label><br>
-                    <input type="text" placeholder="Enter Email" name="email" required><br>
+   <label for="psw"><b>Password</b></label><br>
+   <input type="password" placeholder="Password" id="psw" name="psw"  title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required><br>
                 
-                    <label for="psw"><b>Password</b></label><br>
-                    <input type="password" placeholder="Enter Password" name="psw"  required><br>
-                
-                    <label for="psw-repeat"><b>Repeat Password</b></label><br>
-                    <input type="password" placeholder="Repeat Password" name="psw_r"  required><br>
+   <label for="psw-repeat"><b>Repeat Password</b></label><br>
+  <input type="password" placeholder="Repeat Password" name="psw_r"  required><br>
                      
-                    <label for="location"><b>Location </b></label><br>
-                    <input type="text" placeholder="Location" name="location" id="location" required><br>
+   <label for="location"><b>Location </b></label><br>
+   <input type="text" placeholder="Location" name="location" id="location" required><br>
 
-                    <label for="license"><b>License(pdf/photo) </b></label><br><br>
-                    <input type="file" name="license" id="license">
+   <label for="license"><b>License(pdf/photo) </b></label><br><br>
+   <input type="file" name="license" id="license">
                     <br><br>
 
-                    <label for="qualification"><b>Qualifications</b></label><br>
-                    <input type="text" placeholder="Enter Qualifications" name="qualification" id="qualification" required><br>
+   <label for="qualification"><b>Qualifications</b></label><br>
+   <input type="text" placeholder="Enter Qualifications" name="qualification" id="qualification" required><br>
 
-                    <label for="experience"><b>Experience </b></label><br>
-                      <input type="text" placeholder="Experience" name="experience" required><br>
+   <label for="experience"><b>Experience </b></label><br>
+   <input type="text" placeholder="Experience" name="experience" required><br>
     
 
-                    <br>
-            </td>
+                    <br>           
+</div>
+     </td>
+     </table>             
+                    
+     <b> &nbsp;&nbsp;By creating an account you agree to our <a href="#">Terms & Privacy</a>.</b>
+    <br><br>
 
-        </tr>
-        <tr><td colspan="2">
-            <hr>
-            
-                   <b> &nbsp;&nbsp;&nbsp;By creating an account you agree to our <a href="#">Terms & Privacy</a>.</b>
-                   <div class="latter">
-                    <button type="submit" class="registerbtn" name="regbtn"><b>Register</b></button>
-                </div>
-                  </div>
-        </td>
-        <td></td></tr>
-    </table>
-</form>
+    <input type="submit" name="regbtn" value="Register"> <br>
+
+
+        <br><br>
+      
+
+       
+
+  </form>
+
+  
+</div>
+
 </body>
 </html>
