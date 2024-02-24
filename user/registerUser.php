d78cd9e7116cc7f1cf2b054c4002b667aaf9f2e1
Laki1114 <2021is052@stu.ucsc.cmb.ac.lk>
2024-02-24 21:45:38 +0530

    back end

--- user/registerUser.php
+++ user/registerUser.php
@@ -9,7 +9,7 @@
 
 <div class="bg-img">
   
-  <form action="/action_page.php" class="container">
+  <form class="container" name="user" method="post" action="intermediateUser.php" onsubmit="myFunction()">
   <h1>Register as a User</h1> <br>
     
     <p>Please fill in this form to create an account.</p><br>
@@ -55,7 +55,21 @@
     <label for="psw-repeat"><b>Repeat Password</b></label><br>
     <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required><br>
      </td>
-     </table>             
+
+     
+       
+     </table>     
+     
+    
+
+            <div id="message">
+              <p>Password must contain the following:</p>
+              <li id="letter" class="invalid">A <b>lowercase</b> letter</li>
+              <li id="capital" class="invalid">A <b>capital (uppercase)</b> letter</li>
+              <li id="number" class="invalid">A <b>number</b></li>
+              <li id="length" class="invalid">Minimum <b>8 characters</b></li>
+            </div>
+            <br>
                     
      <b> &nbsp;&nbsp;By creating an account you agree to our <a href="#">Terms & Privacy</a>.</b>
     <br>
@@ -63,7 +77,6 @@
     <input type="submit" value="Register"> <br>
 
 
-        <br><br>
       
 
        
@@ -72,6 +85,70 @@
 
   
 </div>
+<script>
 
+function myFunction() {
+  alert("The form was submitted");
+}
+
+
+  var myInput = document.getElementById("psw");
+  var letter = document.getElementById("letter");
+  var capital = document.getElementById("capital");
+  var number = document.getElementById("number");
+  var length = document.getElementById("length");
+  
+  // When the user clicks on the password field, show the message box
+  myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+  }
+  
+  // When the user clicks outside of the password field, hide the message box
+  myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+  }
+  
+  // When the user starts to type something inside the password field
+  myInput.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {  
+      letter.classList.remove("invalid");
+      letter.classList.add("valid");
+    } else {
+      letter.classList.remove("valid");
+      letter.classList.add("invalid");
+    }
+    
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {  
+      capital.classList.remove("invalid");
+      capital.classList.add("valid");
+    } else {
+      capital.classList.remove("valid");
+      capital.classList.add("invalid");
+    }
+  
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {  
+      number.classList.remove("invalid");
+      number.classList.add("valid");
+    } else {
+      number.classList.remove("valid");
+      number.classList.add("invalid");
+    }
+    
+    // Validate length
+    if(myInput.value.length >= 8) {
+      length.classList.remove("invalid");
+      length.classList.add("valid");
+    } else {
+      length.classList.remove("valid");
+      length.classList.add("invalid");
+    }
+  }
+  </script>
 </body>
 </html>
