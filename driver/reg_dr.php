b127e26dec328b403850056e87b6328d50011c3b
faheemafrauz <frauzfaheema@gmail.com>
2024-02-26 19:43:06 +0530

    Update reg_dr.php

--- driver/reg_dr.php
+++ driver/reg_dr.php
@@ -52,7 +52,7 @@ if(isset($_POST['regbtn'])){
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="registerUser.css">
+<link rel="stylesheet" href="../css/Driver/registerUser.css">
 </head>
 <body>
 
