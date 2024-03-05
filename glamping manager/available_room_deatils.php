5dd76f6f352ab01a00dc6dd135ca03ca40e2d18d
Amayaherath0121 <145965274+Amayaherath0121@users.noreply.github.com>
2024-03-05 13:06:05 +0530

    create available form

---
+++ glamping manager/available_room_deatils.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>available form</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="glm_css_files/available_room_details.css">
+
+</head>
+
+<body>
+
+<div class="status">
+  <div class="number">
+    <button>2</button>
+  </div>
+  <div class="text">
+    Check Availability
+  </div>
+</div>
+    
+<div class="container">
+<div class="title">
+        <h1>Select the type that you want!</h1>
+    </div>
+
+    <div class="tb">
+    <table>
+  <tr>
+  <th>Available Type</th>
+  <th>Type of Meal</th>
+  <th></th>
+  <th>Price</th>
+  </tr>
+  <tr>
+  <td>Luxury Tent</td>
+  <td>Half Board</td>
+  <td>Single</td>
+  <td>$100</td>
+  </tr>
+
+  <tr>
+  <td></td>
+  <td></td>
+  <td>Double</td>
+  <td>$200</td>
+  </tr>
+  
+  <tr>
+  <td></td>
+  <td>Full Board</td>
+  <td>single</td>
+  <td>$100</td>
+  </tr>
+
+  <tr>
+  <td></td>
+  <td></td>
+  <td>Double</td>
+  <td>$150</td>
+  </tr>
+
+  <tr>
+  <td>Family Tent</td>
+  <td>Half Board</td>
+  <td>Triple</td>
+  <td>$200</td>
+  </tr>
+
+  <tr>
+  <td></td>
+  <td></td>
+  <td>Quadruple</td>
+  <td>$150</td>
+  </tr>
+
+  <tr>
+  <td></td>
+  <td>Full Board</td>
+  <td>Triple</td>
+  <td>$200</td>
+  </tr>
+  
+  <tr>
+  <td></td>
+  <td></td>
+  <td>Quadruple</td>
+  <td>$150</td>
+  </tr>
+</table>
+    </div>
+</div>
+
+</body>
+</html>
