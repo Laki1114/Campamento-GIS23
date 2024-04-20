91e8986a040ed41891d2792c5bde2b96dedc0a8c
Laki1114 <2021is052@stu.ucsc.cmb.ac.lk>
2024-04-20 16:17:24 +0530

    admin changes + chat +contact us done

--- home/home.php
+++ home/home.php
@@ -90,57 +90,9 @@ We eagerly await the opportunity to welcome you to Campamento and make your camp
 <?php include 'cards.php'; ?>
 </section>
 
+<?php include 'contactus.php'; ?>
 
 
-
-<section class="about" id="contact">
-
-   <div class="flex">
-
-      <div class="image">
-         <img src="../resource/contact.jpg" alt="">
-      </div>
-
-      <div class="content">
-         <h3>Contact Us</h3>
-         <p?>leave us a message:</p>
-
-
-         <?php 
-                            $Messege = "";
-                            if(isset($_GET['error']))
-                            {
-                                $Messege = " Please Fill in the Blanks ";
-                                echo '<div class="alert alert-danger">'.$Messege.'</div>';
-                            }
-
-                            if(isset($_GET['success']))
-                            {
-                                $Messege = " Your Message Has Been Sent ";
-                                echo '<div class="alert alert-success">'.$Messege.'</div>';
-                            }
-                        
-                        ?>
-
-
-
-         <form action="contact.php">
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" placeholder="Your name..">
-        
-        <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Your E-mail.." name="email" required>
-
-        <label for="subject">Messege</label>
-        <textarea id="subject" name="subject" placeholder="Your Messege" style="height:100px"></textarea>
-        
-        <input type="submit" value="Submit">
-      </form>
-      </div>
-
-   </div>
-
-</section>
 <br><br>
 
 <?php include 'footer.php'; ?>
