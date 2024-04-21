cb89812bd28bc834103c1af72b2976d8d88ccbbb
faheemafrauz <frauzfaheema@gmail.com>
2024-04-21 23:28:58 +0530

    driver

--- driver/reg_dr.php
+++ driver/reg_dr.php
@@ -1,5 +1,5 @@
 <?php
-include '../database/linklinkz.php';
+include 'config.php';
  
 if(isset($_POST['regbtn'])){
 	$f=$_POST['firstname'];
@@ -13,8 +13,11 @@ if(isset($_POST['regbtn'])){
 	$ema=$_POST['email'];
 	$ps=$_POST['psw'];
 	$psw=$_POST['psw_r'];
-    $lo=$_POST['location'];
-    $q=$_POST['qualification'];
+  $lo=$_POST['location'];
+  $type=$_POST['type'];
+  $model=$_POST['model'];
+  $ac=$_POST['nonac'];
+  $district=$_POST['district'];
 
 	if ($ps != $psw) {
 		echo "<script> alert(' Please enter same passwords ') </script>";
@@ -24,26 +27,80 @@ if(isset($_POST['regbtn'])){
 	//code for image uploading
   $targetDir = "pic/";  // Specify the directory where you want to save the uploaded images
   $targetDirr = "license/";
-  $targetDire = "vehicle/";
+  
 
   // Handle the first image
   $targetFile1 = $targetDir . basename($_FILES["picture"]["name"]);
   move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFile1);
 
-  $targetFile2 = $targetDire . basename($_FILES["vehicle"]["name"]);
-  move_uploaded_file($_FILES["vehicle"]["tmp_name"], $targetFile2);
   // Handle the second image
   $targetFile3 = $targetDirr . basename($_FILES["license"]["name"]);
   move_uploaded_file($_FILES["license"]["tmp_name"], $targetFile3);
  
  
   //$password = md5($ps);
+  $sql = "SELECT * FROM driver WHERE email = '$ema'";
+  $result = $con->query($sql);
+  if ($result->num_rows > 0) {
+      // If the email already exists, return an error message
+      $response = array('status' => 'error', 'message' => 'Email already exists. Please use a different email address.');
+      echo json_encode($response);
+      exit();
+  }
 
-  $i="insert into driver(firstname, lastname, gender, phone, address,veh_des, vehicle, picture, experience, nic, email, password, location, license, qualification)values ('$f','$l','$g','$p','$a','$v','$targetFile2','$targetFile1','$e', '$n','$ema','$ps','$lo','$targetFile3', '$q')";
-  if(mysqli_query($linkz, $i)){
+  $i="insert into driver(firstname, lastname, gender, phone, address,veh_des, picture, experience, nic, email, password, location, license, Vehicle_type, Model, AC, district)values ('$f','$l','$g','$p','$a','$v' ,'$targetFile1','$e', '$n','$ema','$ps','$lo','$targetFile3', '$type', '$model','$ac','$district')";
+  if(mysqli_query($con, $i)){
+    // Get the ID of the inserted driver
+    $driver_id = mysqli_insert_id($con);
+    
+    // Handle vehicle images
+    $targetDire = "vehicle/";;  // Specify the directory where you want to save the uploaded images
+
+    // Allowed image formats
+$allowedFormats = array('jpg', 'jpeg', 'png');
+
+if(count($_FILES["vehicle"]["name"]) != 3) {
+  echo "Please upload exactly three vehicle images.";
+} else {
+// Loop through each uploaded vehicle image
+foreach ($_FILES["vehicle"]["tmp_name"] as $key => $tmp_name) {
+    // Get the file extension
+    $fileExt = strtolower(pathinfo($_FILES["vehicle"]["name"][$key], PATHINFO_EXTENSION));
+    
+    // Check if the file format is allowed
+    if (in_array($fileExt, $allowedFormats)) {
+        // Construct the target file path
+        $targetFile2 = $targetDire . basename($_FILES["vehicle"]["name"][$key]);
+        
+        // Move the uploaded image to the target directory
+        if (move_uploaded_file($_FILES["vehicle"]["tmp_name"][$key], $targetFile2)) {
+            // Get file name and file path
+            $fileName = basename($_FILES["vehicle"]["name"][$key]);
+            $filePath = mysqli_real_escape_string($con, $targetFile2);
+            
+            // Insert file name and file path into the 'vehicle_images' table
+            $sql = "INSERT INTO v_images (d_id, file_name, path, uploaded_on) 
+                    VALUES ('$driver_id', '$fileName', '$filePath', NOW())";
+            if(mysqli_query($con, $sql)){
+                // Success
+            } else {
+                echo "Error inserting vehicle image: " . mysqli_error($con);
+            }
+        } else {
+            // Handle upload errors
+            echo "Error uploading vehicle image.";
+        }
+    } else {
+        // Invalid image format
+        echo "Invalid image format. Only JPG, JPEG, and PNG formats are allowed.";
+    }
+}
+
+        
 	$msg = "Account created";
 	header("Location: ../home?warning=" . urlencode($msg));
 	exit();
+    }
   }
 }
 ?>
@@ -92,11 +149,19 @@ if(isset($_POST['regbtn'])){
       <label for="photo"><b>Picture </b></label><br><br>
       <input type="file" name="picture" id="pic"><br><br>
 
+      <label for="type"><b>Vehicle Type</b></label><br>
+     <select id="type" name="type" required>
+     <option value="" disabled selected>Select</option>
+    <option>Car/ Mini SUV(3-4)</option>
+      <option>Mini Van(6-7)</option>
+      <option>Van(8-9)</option>
+     </select><br>
+
       <label for="vehicle-des"><b>Vehicle Description </b></label><br>
        <input type="text" placeholder="Vehicle des" name="vehicleDes" required><br>
 
       <label for="Vehicle"><b>Vehicle Pictures </b></label><br><br>
-      <input type="file" name="vehicle" id="vehicle">
+      <input type="file" name="vehicle[]" id="vehicle" multiple accept="image/*" onchange="limitFiles(this)">
 
 </div>
       </td>
@@ -116,7 +181,7 @@ if(isset($_POST['regbtn'])){
    <input type="password" placeholder="Password" id="psw" name="psw"  title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required><br>
                 
    <label for="psw-repeat"><b>Repeat Password</b></label><br>
-  <input type="password" placeholder="Repeat Password" name="psw_r"  required><br>
+   <input type="password" placeholder="Repeat Password" name="psw_r"  required><br>
                      
    <label for="location"><b>Location </b></label><br>
    <input type="text" placeholder="Location" name="location" id="location" required><br>
@@ -125,15 +190,49 @@ if(isset($_POST['regbtn'])){
    <input type="file" name="license" id="license">
                     <br><br>
 
-   <label for="qualification"><b>Qualifications</b></label><br>
-   <input type="text" placeholder="Enter Qualifications" name="qualification" id="qualification" required><br>
+   <label for="model"><b>Vehicle Model</b></label><br>
+   <input type="text" placeholder="Enter model" name="model" id="model" required><br>
 
+   <label for="nonac"><b>A/C or Non A/C</b></label><br>
+    <select id="nonac" name="nonac" required>
+    <option value="" disabled selected>Select</option>
+    <option>A/C</option>
+    <option>Non A/C</option>
+    </select><br>
+   
    <label for="experience"><b>Experience </b></label><br>
    <input type="text" placeholder="Experience" name="experience" required><br>
-    
+   
+   <label for="district">Select your district:</label>
+<select id="district" name="district">
+  <option value="Ampara">Ampara</option>
+  <option value="Anuradhapura">Anuradhapura</option>
+  <option value="Badulla">Badulla</option>
+  <option value="Batticaloa">Batticaloa</option>
+  <option value="Colombo">Colombo</option>
+  <option value="Galle">Galle</option>
+  <option value="Gampaha">Gampaha</option>
+  <option value="Hambantota">Hambantota</option>
+  <option value="Jaffna">Jaffna</option>
+  <option value="Kalutara">Kalutara</option>
+  <option value="Kandy">Kandy</option>
+  <option value="Kegalle">Kegalle</option>
+  <option value="Kilinochchi">Kilinochchi</option>
+  <option value="Kurunegala">Kurunegala</option>
+  <option value="Mannar">Mannar</option>
+  <option value="Matale">Matale</option>
+  <option value="Matara">Matara</option>
+  <option value="Monaragala">Monaragala</option>
+  <option value="Mullaitivu">Mullaitivu</option>
+  <option value="Nuwara Eliya">Nuwara Eliya</option>
+  <option value="Polonnaruwa">Polonnaruwa</option>
+  <option value="Puttalam">Puttalam</option>
+  <option value="Ratnapura">Ratnapura</option>
+  <option value="Trincomalee">Trincomalee</option>
+  <option value="Vavuniya">Vavuniya</option>
+</select>
 
-                    <br>           
-</div>
+   </div>
      </td>
      </table>             
                     
@@ -152,6 +251,15 @@ if(isset($_POST['regbtn'])){
 
   
 </div>
-
+<script>
+function limitFiles(input) {
+    if (input.files.length > 3) {
+        alert("You can only upload a maximum of three images.");
+        // Clear the selected files
+        input.value = '';
+    }
+}
+</script>
+s
 </body>
 </html>
